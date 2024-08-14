56ea35cd5a29e06690445db6f9f378ee2db8d194
VictorMarimon <3reehuy@gmail.com>
2024-08-14 14:03:20 -0500

    refactor: :recycle: se agregan y se modifican la manipulación de los datos del JSON

--- js/principal.js
+++ js/principal.js
@@ -24,16 +24,12 @@ document.addEventListener('DOMContentLoaded', (e)=>{
                 bandera++;
                 artista = artista + bandera;
 
-                console.log(vinilosBD[artista][0])
-                console.log(vinilosBD[artista][1])
-                console.log(vinilosBD[artista][2])
-                console.log(vinilosBD[artista][3])
-
                 const conteniPagPrincipal = document.getElementById('contenido_principal');
 
                 const viniloNuevo = document.createElement('div');
                 const imagenViniloNuevo = document.createElement('img');
                 const descripcionViniloNuevo = document.createElement('p');
+                const imagenViniloNuevoModal = document.createElement('a');
                 const precioViniloNuevo = document.createElement('label');
                 const signoViniloNuevo = document.createElement('span');
 
@@ -43,6 +39,8 @@ document.addEventListener('DOMContentLoaded', (e)=>{
                 imagenViniloNuevo.setAttribute('src', vinilosBD[artista][0]);
                 imagenViniloNuevo.setAttribute('alt', vinilosBD[artista][1]);
 
+                imagenViniloNuevoModal.append(imagenViniloNuevo);
+
                 descripcionViniloNuevo.classList.add('contenido__sección__descripción');
                 descripcionViniloNuevo.innerText = vinilosBD[artista][2];
 
@@ -50,78 +48,36 @@ document.addEventListener('DOMContentLoaded', (e)=>{
                 signoViniloNuevo.innerText = '$';
 
                 precioViniloNuevo.appendChild(signoViniloNuevo);
-                precioViniloNuevo.innerText = vinilosBD[artista][3];
+                precioViniloNuevo.innerText = "$ " +vinilosBD[artista][3];
 
-                viniloNuevo.append(imagenViniloNuevo);
+                viniloNuevo.append(imagenViniloNuevoModal);
                 viniloNuevo.append(descripcionViniloNuevo);
                 viniloNuevo.append(precioViniloNuevo);
 
                 conteniPagPrincipal.appendChild(viniloNuevo);
 
                 artista = "artista_"
+
+                //Dinamismo del modal (principal.html)
+
+                const vinilo = document.getElementsByClassName('contenido__sección');
+                const modal = document.getElementById('modal');
+
+
+                imagenViniloNuevoModal.addEventListener('click',(e)=>{
+
+                    imagenViniloNuevoModal.setAttribute('href', '#modal');
+
+                    for (const vinilosModalBD of valor.vinilosModal) {
+                        const modalIndividual = modal.querySelector('div').querySelector('p');
+
+                        modalIndividual.innerText = vinilosModalBD[artista][0]; 
+                        console.log(vinilosModalBD);
+                    }
+                })
             }
         }
     }, 200);
     
 
 });
-
-
-//prueba
-
-const pruebaBoton = document.querySelector('.contenido__sección__imagen');
-
-const shakira = document.getElementById('shakira');
-
-shakira.addEventListener('click', ()=>{
-    console.log("hola")
-})
-
-console.log(pruebaBoton)
-
-pruebaBoton.addEventListener('click', (e)=>{
-    //prueba
-
-    const conteniPagPrincipal = document.getElementById('contenido_principal');
-
-    const viniloNuevo = document.createElement('div');
-    const imagenViniloNuevo = document.createElement('img');
-    const descripcionViniloNuevo = document.createElement('p');
-    const precioViniloNuevo = document.createElement('label');
-    const signoViniloNuevo = document.createElement('span');
-
-    viniloNuevo.classList.add('contenido__sección');
-    
-    imagenViniloNuevo.classList.add('contenido__sección__imagen');
-    imagenViniloNuevo.setAttribute('src', "hola");
-    imagenViniloNuevo.setAttribute('alt', "hola");
-
-    descripcionViniloNuevo.classList.add('contenido__sección__descripción');
-    descripcionViniloNuevo.innerText = "hola";
-
-    signoViniloNuevo.classList.add('contenido__sección--peso');
-    signoViniloNuevo.innerText = '$';
-
-    precioViniloNuevo.append(signoViniloNuevo);
-    precioViniloNuevo.innerText = "20.000"
-
-    viniloNuevo.append(imagenViniloNuevo);
-    viniloNuevo.append(descripcionViniloNuevo);
-    viniloNuevo.append(precioViniloNuevo);
-
-    conteniPagPrincipal.appendChild(viniloNuevo);
-})
-
-
-
-//Dinamismo del modal (principal.html)
-
-const vinilo = document.getElementsByClassName('contenido__sección');
-const imagenVinilo = document.getElementsByClassName('contenido__sección__imagen');
-
-
-for (const valor of imagenVinilo) {
-    valor.addEventListener('click', ()=>{
-        
-    });
-}
