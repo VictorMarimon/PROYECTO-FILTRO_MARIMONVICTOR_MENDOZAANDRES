a1dcf9aae2e07699b115277147be89393b2b1a2d
VictorMarimon <3reehuy@gmail.com>
2024-08-29 13:45:29 -0500

    refactor: :recycle: Se realiza el cambio de credenciales

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ const user = document.querySelector('#username');
 const password = document.getElementById('password');
 const btnInicioSesion = document.getElementById('btnInicioSesión');
 
-const USUARIO = "USER1";
-const CONTRASEÑA = "2024USER";
+const USUARIO = "2024";
+const CONTRASEÑA = "2024";
 
 btnInicioSesion.addEventListener('click',()=>{
     if(user.value === USUARIO && password.value === CONTRASEÑA){
